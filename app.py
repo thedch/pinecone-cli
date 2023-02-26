05360966477fc872a089407e1c79d27e167d8949
nicrso <n.ouporov@columbia.edu>
2023-02-25 17:47:03 -0800

    Merged with main

--- app.py
+++ app.py
@@ -16,18 +16,25 @@ top        Text  Seek  https://www.seek.ai/  $2,000,000  Y Combinator    2021  S
 freq        132     1                     1          18             8     106                 75               12
 """
 
-def get_data(num_names_per_category=2):
+def get_data(num_names_per_category=2, num_categories=3):
     # The following code reads the CSV and returns it the way I want it
+
+    num_categories = min(num_categories, 14)
+
     data = pd.read_csv('./market_map.csv')
     out = defaultdict(list)
-    for category in data['Category'].value_counts().index[:2]:
+    for category in data['Category'].value_counts().index[:num_categories]:
         random_names = data[data['Category'] == category].sample(num_names_per_category)
         out[category] = random_names['Name'].values.tolist()
 
     return out
 
 
-def make_plot(plot_type):
+def make_plot(category, num_categories, num_names_per_category):
+
+    num_categories = int(num_categories)
+    num_names_per_category = int(num_names_per_category)
+
     del plot_type
 
     pinecone.init(
@@ -38,7 +45,7 @@ def make_plot(plot_type):
     print(index)
     import pdb; pdb.set_trace()
 
-    data = get_data()
+    data = get_data(num_names_per_category, num_categories)
     names = []
     parents = []
     for category, names_list in data.items():
@@ -49,7 +56,6 @@ def make_plot(plot_type):
     names += unique_parents
     parents += [''] * len(unique_parents)
 
-    print(names, parents)
     fig = px.treemap(
         names=names,
         parents=parents,
@@ -60,8 +66,11 @@ def make_plot(plot_type):
     return fig
 
 with gr.Blocks() as demo:
-    button = gr.Radio(label="Plot type", choices=['generative AI'], value='scatter_plot')
+    category = gr.Textbox(label='Market Category')
+    n_subcategories = gr.Number(label="Number of Subcategories")
+    n_companies = gr.Number(label="Number of Companies per Subcategory")
+    button = gr.Button("Generate")
     plot = gr.Plot(label="Plot")
-    button.change(make_plot, inputs=button, outputs=[plot])
-    demo.load(make_plot, inputs=[button], outputs=[plot])
+    button.click(make_plot, inputs=[category, n_subcategories, n_companies], outputs=[plot])
+    # demo.load(make_plot, inputs=[button, category], outputs=[plot])
     demo.launch()
