960f0605711806c061fde614168d65d562dabbcb
Daniel <13357734+thedch@users.noreply.github.com>
2023-02-25 16:53:26 -0800

    Treemap works

--- app.py
+++ app.py
@@ -1,61 +1,131 @@
+import plotly.express as px
+import altair as alt
 import gradio as gr
+import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
-from datasets import load_dataset
+from vega_datasets import data
 
-dataset = load_dataset("gradio/NYC-Airbnb-Open-Data", split="train")
-df = dataset.to_pandas()
 
-def filter_map(min_price, max_price, boroughs):
+def make_plot(plot_type):
+    if plot_type == "scatter_plot":
+        cars = data.cars()
+        return alt.Chart(cars).mark_point().encode(
+            x='Horsepower',
+            y='Miles_per_Gallon',
+            color='Origin',
+        )
+    elif plot_type == "heatmap":
+        # Compute x^2 + y^2 across a 2D grid
+        x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+        z = x ** 2 + y ** 2
 
-    filtered_df = df[(df['neighbourhood_group'].isin(boroughs)) &
-          (df['price'] > min_price) & (df['price'] < max_price)]
-    names = filtered_df["name"].tolist()
-    prices = filtered_df["price"].tolist()
-    text_list = [(names[i], prices[i]) for i in range(0, len(names))]
-    fig = go.Figure(go.Scattermapbox(
-            customdata=text_list,
-            lat=filtered_df['latitude'].tolist(),
-            lon=filtered_df['longitude'].tolist(),
-            mode='markers',
-            marker=go.scattermapbox.Marker(
-                size=6
-            ),
-            hoverinfo="text",
-            hovertemplate='<b>Name</b>: %{customdata[0]}<br><b>Price</b>: $%{customdata[1]}'
-        ))
+        # Convert this grid to columnar data expected by Altair
+        source = pd.DataFrame({'x': x.ravel(),
+                            'y': y.ravel(),
+                            'z': z.ravel()})
+        return alt.Chart(source).mark_rect().encode(
+            x='x:O',
+            y='y:O',
+            color='z:Q'
+        )
+    elif plot_type == "us_map":
+        fig = px.treemap(
+            names = ["Eve","Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+            parents = ["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve"]
+        )
+        fig.update_traces(root_color="lightgrey")
+        fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+        return fig
+        # fig.show()
+    elif plot_type == "interactive_barplot":
+        source = data.movies.url
 
-    fig.update_layout(
-        mapbox_style="open-street-map",
-        hovermode='closest',
-        mapbox=dict(
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=40.67,
-                lon=-73.90
-            ),
-            pitch=0,
-            zoom=9
-        ),
-    )
+        pts = alt.selection(type="single", encodings=['x'])
 
-    return fig
+        rect = alt.Chart(data.movies.url).mark_rect().encode(
+            alt.X('IMDB_Rating:Q', bin=True),
+            alt.Y('Rotten_Tomatoes_Rating:Q', bin=True),
+            alt.Color('count()',
+                scale=alt.Scale(scheme='greenblue'),
+                legend=alt.Legend(title='Total Records')
+            )
+        )
+
+        circ = rect.mark_point().encode(
+            alt.ColorValue('grey'),
+            alt.Size('count()',
+                legend=alt.Legend(title='Records in Selection')
+            )
+        ).transform_filter(
+            pts
+        )
+
+        bar = alt.Chart(source).mark_bar().encode(
+            x='Major_Genre:N',
+            y='count()',
+            color=alt.condition(pts, alt.ColorValue("steelblue"), alt.ColorValue("grey"))
+        ).properties(
+            width=550,
+            height=200
+        ).add_selection(pts)
+
+        plot = alt.vconcat(
+            rect + circ,
+            bar
+        ).resolve_legend(
+            color="independent",
+            size="independent"
+        )
+        return plot
+    elif plot_type == "radial":
+        source = pd.DataFrame({"values": [12, 23, 47, 6, 52, 19]})
+
+        base = alt.Chart(source).encode(
+            theta=alt.Theta("values:Q", stack=True),
+            radius=alt.Radius("values", scale=alt.Scale(type="sqrt", zero=True, rangeMin=20)),
+            color="values:N",
+        )
+
+        c1 = base.mark_arc(innerRadius=20, stroke="#fff")
+
+        c2 = base.mark_text(radiusOffset=10).encode(text="values:Q")
+
+        return c1 + c2
+    elif plot_type == "multiline":
+        source = data.stocks()
+
+        highlight = alt.selection(type='single', on='mouseover',
+                                fields=['symbol'], nearest=True)
+
+        base = alt.Chart(source).encode(
+            x='date:T',
+            y='price:Q',
+            color='symbol:N'
+        )
+
+        points = base.mark_circle().encode(
+            opacity=alt.value(0)
+        ).add_selection(
+            highlight
+        ).properties(
+            width=600
+        )
+
+        lines = base.mark_line().encode(
+            size=alt.condition(~highlight, alt.value(1), alt.value(3))
+        )
+
+        return points + lines
+
 
 with gr.Blocks() as demo:
-    with gr.Column():
-        with gr.Row():
-            # min_price = gr.Number(value=250, label="Minimum Price")
-            # max_price = gr.Number(value=1000, label="Maximum Price")
+    button = gr.Radio(label="Plot type",
+                      choices=['scatter_plot', 'heatmap', 'us_map',
+                               'interactive_barplot', "radial", "multiline"], value='scatter_plot')
+    plot = gr.Plot(label="Plot")
+    button.change(make_plot, inputs=button, outputs=[plot])
+    demo.load(make_plot, inputs=[button], outputs=[plot])
 
-            search_query = gr.Textbox(label="Category")
-            num_categories = gr.Number(value=10, label="Number of Subcategories")
-            num_companies = gr.Number(value=5, label="Number of Companies")
-
-        boroughs = gr.CheckboxGroup(choices=["Queens", "Brooklyn", "Manhattan", "Bronx", "Staten Island"], value=["Queens", "Brooklyn"], label="Select Boroughs:")
-        btn = gr.Button(value="Update Filter")
-        map = gr.Plot().style()
-    demo.load(filter_map, [min_price, max_price, boroughs], map)
-    btn.click(filter_map, [min_price, max_price, boroughs], map)
 
 if __name__ == "__main__":
     demo.launch()
